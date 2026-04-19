410d2bba19bce94b98b4e7eea095ec3baa393d07
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-04-19 16:04:41 -0700

    finished practice tests

--- src/Practice.java
+++ src/Practice.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Practice {
     /**
      * Returns the difference between the largest and smallest integer in an array.
@@ -6,12 +10,65 @@ public class Practice {
      * @return the difference between the largest and smallest number
      */
     public static int maxDiff(int[] nums) {
-        // TODO: implement this
-        return -1;
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            if (num > max) {
+                max = num;
+            } else if (num < min) {
+                min = num;
+            }}
+        return max - min;
     }
 
+    public static String longestWord(ArrayList<String> words, char letter){
+        String longWord = "";
+        for (String word : words){
+            if (word.startsWith(String.valueOf(letter)) && word.length() > longWord.length()) {
+                longWord = word;
+            }
+        }
 
-    // TODO: Implement the other methods from the README AND tests for each one
+        return longWord;
+    }
+
+    public static int countWords(HashSet<String> words, int lengthOne, int lengthTwo){
+        int wordCount = 0;
+        for (String word : words) {
+            if ( word.length() >= lengthOne && word.length() <= lengthTwo){
+                wordCount++;
+            }}
+            return wordCount;
+    }
+
+    public static int countDifference(HashMap<String, Integer> words){
+        int odd = 0;
+        int even = 0;
+        for (int word : words.values()) {
+            if(word % 2 == 0){
+                even++;
+            }else {
+                odd++;
+            }
+        } return odd - even;
+    }
+
+    public static int secondLargest(HashMap<Integer, String> lengths) {
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for (int key : lengths.keySet()){
+            if (key > max){
+                secondMax = max;
+                max = key;
+            } else if ( key > secondMax) {
+                secondMax = key;
+            }
+        }
+
+        return secondMax;
+
+    }
 
     // For each method you are only required to implement it for one of the data
     // structures. But use a different data structure for each method. For example,
--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,10 +1,15 @@
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class PracticeTest {
 
-    // TODO: Make more tests for maxDiff
+    // maxDiff
     @Test
     void testMaxDiffPositiveAndNegative() {
         // Arrange
@@ -17,10 +22,166 @@ public class PracticeTest {
         // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
         assertEquals(14, actual);
     }
+     
+    @Test
+    void allSameNumbers() {
+        //Arrange
+        int[] numbers = {9,9,9,9};
+        // Act
+        int actual = Practice.maxDiff(numbers);
+        // Assert
+        // Largest: 9, Smallest: 9, Difference: 9 - 9 = 0
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void singleElement() {
+        //Arrange
+        int[] numbers = {5};
+        // Act
+        int actual = Practice.maxDiff(numbers);
+        // Assert
+        // Largest: 5, Smallest: 5, Difference: 5 - 5 = 0
+        assertEquals(0, actual);
+    }
     
 
-    // TODO: Make tests for each problem you solve
+    // longestWord()
+
+    @Test
+    void normalCase() {
+        //Arrange
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("car", "carpenter", "couch", "tools", "tablepool"));
+        //Act
+        String actual = Practice.longestWord(words, 'c');
+        //Assert
+        assertEquals("carpenter", actual);
+    }
+
+    @Test
+    void nonStartingWithTheSpecifiedLetter () {
+        //Arrange
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("car", "carpenter", "couch", "tools", "tablepool"));
+        //Act
+        String actual = Practice.longestWord(words, 'p');
+        //Assert
+        assertEquals("", actual);
+    }
+
+    //countWords()
+    @Test
+    void normalCaseReturnsAccurateWordCount() {
+        //Arrange
+        HashSet<String> words = new HashSet<>(Arrays.asList("cat", "combination", "carpet", "purse", "couch"));
+        //Act
+        int actual = Practice.countWords(words, 3, 5);
+        //Assert
+        assertEquals(3, actual);
+    }
+
+    @Test
+    void noWordsBetweenLengths() {
+        //Arrange
+        HashSet<String> words = new HashSet<>(Arrays.asList("cat", "combination", "carpet", "purse", "couch"));
+        //Act
+        int actual = Practice.countWords(words, 7, 9);
+        //Assert
+        assertEquals(0, actual);
+    }
+
+    //countDifference()
+    @Test
+    void normalCaseWithMixOfOddAndEven() {
+        //Arrange
+        HashMap<String, Integer> words = new HashMap<>();
+        words.put("cat", 3);
+        words.put("combination", 11);
+        words.put("carpet", 6);
+        words.put("purse", 5);
+        words.put("couch", 5);
+        //Act
+        int actual = Practice.countDifference(words);
+        //Assert
+        assertEquals(3, actual);
+    }
+
+    @Test 
+    void allEvenNumbers(){
+        //Arrange
+        HashMap<String, Integer> words = new HashMap<>();
+        words.put("cats", 4);
+        words.put("comb", 4);
+        words.put("carpet", 6);
+        words.put("flower", 6);
+        words.put("book", 4);
+        //Act
+        int actual = Practice.countDifference(words);
+        //Assert
+        assertEquals(-5, actual);
+    }
+
+    @Test 
+    void allOddNumbers(){
+        //Arrange
+        HashMap<String, Integer> words = new HashMap<>();
+        words.put("cat", 3);
+        words.put("combination", 11);
+        words.put("car", 3);
+        words.put("peony", 5);
+        words.put("blanket", 7);
+        //Act
+        int actual = Practice.countDifference(words);
+        //Assert
+        assertEquals(5, actual);
+    }
     
+
+    //secondLargest()
+    @Test 
+    void normalCaseToSeeIfItGetsExpectedResult(){
+        //Arrange
+        HashMap<Integer, String> lengths = new HashMap<>();
+        lengths.put(3, "cat");
+        lengths.put(11, "combination");
+        lengths.put(4, "cart");
+        lengths.put(5, "peony");
+        lengths.put(7, "blanket");
+
+        //Act
+    int actual = Practice.secondLargest(lengths);
+    //Assert
+    assertEquals(7, actual);
+    }
+
+    @Test
+    void allNumbersTheSame (){
+        //Arrange
+        HashMap<Integer, String> lengths = new HashMap<>();
+        lengths.put(3, "cat");
+        lengths.put(5, "carts");
+
+        //Act
+    int actual = Practice.secondLargest(lengths);
+    //Assert
+    assertEquals(3, actual);
+    
+    }
+
+    @Test
+    void negativeKeys(){
+        //Arrange
+        HashMap<Integer, String> lengths = new HashMap<>();
+        lengths.put(-1, "cat");
+        lengths.put(-3,"car");
+        lengths.put(-5, "pillow");
+
+        //Act
+        int actual = Practice.secondLargest(lengths);
+
+        //Assert
+        assertEquals(-3, actual);
+    }
+     
 }
 
 
